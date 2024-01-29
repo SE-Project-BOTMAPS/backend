a7412ed356fcc25c0a043122e237aaae91ca0bd1
Quercussi <thsomsits.brie@gmail.com>
2024-01-29 21:31:55 +0700

    Room Code

--- utils/fetchData/roomCode.go
+++ utils/fetchData/roomCode.go
@@ -2,7 +2,6 @@ package fetchData
 
 import (
 	"fmt"
-	"log"
 	"sort"
 	"time"
 
@@ -12,22 +11,24 @@ import (
 
 type DayCourseMap map[string][]models.Course
 
-func RoomCode(room_code string, db *gorm.DB) (DayCourseMap, error) {
+func RoomCode(room_code string, db *gorm.DB) (DayCourseMap, []models.Office, error) {
 	
 	var locations []models.Location
 	var courses []models.Course
 	emptymap := DayCourseMap{}
+	emptyoffices := []models.Office{}
 
 	regexp := "%" + room_code + "%"
+
 
 	// Query all locations with the room code
 	err1 := db.Where("Location LIKE ?", regexp).Find(&locations).Error
 	if len(locations) == 0 {
 		message := "No such room found: " + room_code
-		return emptymap, fmt.Errorf("%w: %s", gorm.ErrRecordNotFound, message)
+		return emptymap, emptyoffices, fmt.Errorf("%w: %s", gorm.ErrRecordNotFound, message)
 	}
 	if err1 != nil {
-		return emptymap, err1
+		return emptymap, emptyoffices, err1
 	}
 
 	// Retrieve location IDs
@@ -35,12 +36,20 @@ func RoomCode(room_code string, db *gorm.DB) (DayCourseMap, error) {
 	for _,location := range locations {
 		locationIds = append(locationIds,location.ID)
 	}
+
+	// Note: After this line, the searching location must exist.
+
+	// Query the owner of the office
+	var offices []models.Office
+	err2 := db.Preload("Location").Where("location_id IN ?", locationIds).Find(&offices).Error
+	if err2 != nil {
+		offices = emptyoffices
+	}
 	
 	// Query all courses with the location ID
-	err2 := db.Preload("Location").Preload("Professor").Where("location_id IN ?", locationIds).Find(&courses).Error
-	if err2 != nil {
-		log.Println(err2)
-		return emptymap, err2
+	err3 := db.Preload("Location").Preload("Professor").Where("location_id IN ?", locationIds).Find(&courses).Error
+	if err3 != nil {
+		return emptymap, offices, nil
 	}
 
 	// categorizing
@@ -66,22 +75,26 @@ func RoomCode(room_code string, db *gorm.DB) (DayCourseMap, error) {
 	}
 	
 	for _, course := range courses {
-		startTime, err := time.Parse("2006-01-02T15:04:05-07:00", course.StartTime)
+		startTime, err := time.Parse(time.RFC3339, course.StartTime)
+		endTime, _ := time.Parse(time.RFC3339, course.EndTime)
+
 		if err != nil {
 			dayCourseMap["na"] = append(dayCourseMap["na"], course)
 			continue 
 		}
 
 		key := daysMapping[startTime.Weekday().String()]
+
+		course.StartTime = startTime.Format("15:04")
+		course.EndTime = endTime.Format("15:04")
+
 		dayCourseMap[key] = append(dayCourseMap[key], course)
 	}
 
 	// sorting by start time
 	sortCoursesByStartTime := func(courses []models.Course) {
 		sort.Slice(courses, func(i, j int) bool {
-            timeI, _ := time.Parse("2006-01-02T15:04:05-07:00", courses[i].StartTime)
-            timeJ, _ := time.Parse("2006-01-02T15:04:05-07:00", courses[j].StartTime)
-            return timeI.Format("15:04:05") < timeJ.Format("15:04:05")
+            return courses[i].StartTime < courses[j].StartTime
 		})
 	}
 
@@ -93,5 +106,5 @@ func RoomCode(room_code string, db *gorm.DB) (DayCourseMap, error) {
 		sortCoursesByStartTime(day)
 	}
 
-	return dayCourseMap, nil
+	return dayCourseMap, offices, nil
 }
