338e538f6db6b946d1070536eea861683743a6fd
Quercussi <thsomsits.brie@gmail.com>
2024-02-19 20:38:46 +0700

    Support Thai Language Search

--- controllers/updateData.go
+++ controllers/updateData.go
@@ -1,17 +1,15 @@
 package controllers
 
 import (
-	"os"
-
 	"github.com/SE-Project-BOTMAPS/backend/utils/fetchData"
 	"github.com/gin-gonic/gin"
 )
 
 func (db *DbController) UpdateData(c *gin.Context) {
-	var events fetchData.Events
-	baseUrl := os.Getenv("BASE_URL") + "events?startDate=2024-01-22&endDate=2024-01-28"
-	fetchData.FetchImprove(baseUrl, &events)
-	fetchData.InsertCourse(events, db.Database)
+	// var events fetchData.Events
+	// baseUrl := os.Getenv("BASE_URL") + "events?startDate=2024-01-22&endDate=2024-01-28"
+	// fetchData.FetchImprove(baseUrl, &events)
+	// fetchData.InsertCourse(events, db.Database)
 	fetchData.InsertOffice(db.Database)
 	c.JSON(200, gin.H{
 		"message": "Data updated",
--- models/profressor_m.go
+++ models/profressor_m.go
@@ -4,6 +4,7 @@ type Professor struct {
 	ID               int64    `json:"id" orm:"auto" gorm:"primary_key;index"`
 	DataWho          string   `json:"data_who" orm:"size(128)" gorm:"unique"`
 	FullName         string   `json:"full_name" orm:"size(128)"`
+	NativeName       string   `json:"native_name" orm:"size(128)"`
 	OfficeLocationID int64    `json:"office_location_id" orm:"size(128)" gorm:"index"`
 	OfficeLocation   Location `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
 	course           []Course
