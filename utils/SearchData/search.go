3158b983998d04a507ff6a62acb351a0c6c55d8e
Quercussi <thsomsits.brie@gmail.com>
2024-02-27 20:09:28 +0700

    Optimized Search API

--- utils/SearchData/search.go
+++ utils/SearchData/search.go
@@ -2,6 +2,7 @@ package searchData
 
 import (
 	"fmt"
+	"log"
 	"strings"
 	"time"
 
@@ -11,67 +12,35 @@ import (
 
 func Search(keyword string, db *gorm.DB) ([]models.Course, error){
 
-	var professorIds []int64
-	var roomIds []int64
-	var courseIds []int
-	var courses []models.Course
-	
 	regexp := "%" + strings.ToLower(keyword) + "%"
 
-	err1 := db.Table("professors").
-			   Where("LOWER(data_who) LIKE ? OR LOWER(full_name) LIKE ? OR native_name LIKE ?", regexp, regexp, regexp).
-			   Select("professors.ID").
-			   Find(&professorIds).Error
-	if(err1 != nil) {
-		return nil,err1
-	}
+	professorClause := "professor_id IN (SELECT ID FROM professors WHERE LOWER(data_who) LIKE ? OR LOWER(full_name) LIKE ? OR native_name LIKE ?)"
+	locationClause := "location_id IN (SELECT ID FROM locations WHERE LOWER(Location) LIKE ?)"
+	courseIDClause := "course_id IN (SELECT course_id FROM course_titles WHERE LOWER(full_title_eng) LIKE ? OR full_title_tha LIKE ?)"
+	queryParameters := fmt.Sprintf("title LIKE ? OR %s OR %s OR %s ",professorClause,locationClause,courseIDClause)
+	args := []interface{}{regexp, regexp, regexp, regexp, regexp, regexp, regexp}
 
-	err2 := db.Table("locations").Where("LOWER(Location) LIKE ?", regexp).Select("locations.ID").Find(&roomIds).Error
-	if(err2 != nil) {
-		return nil,err2
-	}
-
-	err3 := db.Table("course_titles").
-			   Where("LOWER(full_title_eng) LIKE ? OR full_title_tha LIKE ?", regexp, regexp).
-			   Select("course_titles.course_id").
-			   Find(&courseIds).Error
-	if(err3 != nil) {
-		return nil,err3
-	}
-
-	queryParam := "title LIKE ?"
-	var args []interface{}
-	args = append(args, regexp)
-	
-	if len(professorIds) > 0 {
-		queryParam += " OR professor_id IN (?)"
-		args = append(args, professorIds)
-	}
-
-	if len(roomIds) > 0 {
-		queryParam += " OR location_id IN (?)"
-		args = append(args, roomIds)
-	}
-	
-	if len(courseIds) > 0 {
-		queryParam += " OR course_id IN (?)"
-		args = append(args, courseIds)
-	}
-
-	err4 := db.Order("start_time").Preload("Professor").Preload("Location").Where(queryParam, args...).Find(&courses).Error
-	if(err4 != nil) {
-		return nil,err4
+	// Query for courses matching the keyword and related entities
+	var courses []models.Course
+	err := db.Order("start_time").
+			Preload("Professor").
+			Preload("Location").
+			Where(queryParameters, args...).
+			Find(&courses).Error
+	if err != nil {
+		log.Println("Error querying selected courses: ", err)
+		return nil, err
 	}
 
 	for i,course := range(courses) {
 		StartTime, err := time.Parse(time.RFC3339, course.StartTime)
 		if err != nil {
-			fmt.Println("Error parsing start time:", err)
+			log.Println("Error parsing start time:", err)
 		}
 
 		EndTime, err := time.Parse(time.RFC3339, course.EndTime)
 		if err != nil {
-			fmt.Println("Error parsing end time:", err)
+			log.Println("Error parsing end time:", err)
 		}
 		
 		day := StartTime.Weekday().String()
